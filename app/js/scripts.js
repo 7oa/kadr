9cb4b5bb4fd07db5f900c96095b92e5e4c0ffe7a
Ann Semenova <semenova.7oa@yandex.ru>
2018-05-30 17:30:56 +0300

    first

---
+++ app/js/scripts.js
@@ -0,0 +1,7 @@
+// Plugins
+//= plugins/_jquery-3.2.0.min.js
+//= plugins/_slick.min.js
+
+//Scripts
+//= scripts/script.js
+//
---
+++ app/js/scripts/script.js
@@ -0,0 +1,13 @@
+// js jquery
+$(document).ready(function() {
+    $('.index-cat__slider').slick({
+        infinite: true,
+        slidesToShow: 7,
+        slidesToScroll: 1
+    });
+    $('.services-prev__slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    });
+});
